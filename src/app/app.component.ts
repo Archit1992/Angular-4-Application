e473894eaae5398b8a5818426629bdda36594c99
Archit1992 <gajjararchit00004@gmail.com>
2017-05-14 19:51:21 -0400

    adding Project structure of Angular4.

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// logic import here in typescript.
+export class AppComponent {
+  title = "Please enter your name ?";
+  name="";
+}
