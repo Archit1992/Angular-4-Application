ba9a2f1acaad934405eaa5a5bf41654b04e31e10
Archit1992 <gajjararchit00004@gmail.com>
2017-05-19 12:20:25 -0400

    adding new component -> servers <- using Angular CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-// logic import here in typescript.
 export class AppComponent {
   title = "Please enter your name ?";
-  name="";
+  name = "";
 }
